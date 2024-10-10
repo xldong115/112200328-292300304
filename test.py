51fa09218774f9c4fe36ce75f20d8f606180d1eb
xldong115 <141535043+xldong115@users.noreply.github.com>
2024-10-10 08:53:25 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for root, dirs, files in os.walk('.'):
-    level = root.replace('.', '').count(os.sep)
-    indent = ' ' * 4 * (level)
-    print(f"{indent}{os.path.basename(root)}/")
-    for f in files:
-        print(f"{indent}    {f}")
